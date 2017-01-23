91b644659bab03eb7bad140c711b5589db45fd34
moghbaie <moghbaie@stevens.edu>
2017-01-22 20:59:21 -0500

    server&ui

--- Server.R
+++ Server.R
@@ -9,17 +9,17 @@ library(xts)
 library(corrplot)
 library(psych)
 library(hydroTSM)
-#~/Documents/FE541/blockchain-github
+#~/Documents/FE541/bitcoin-deflation
 #~/Documents/FE541/shiny/
-bc <- read.csv("~/Documents/FE541/blockchain-github/bit_usd.csv", header=TRUE)
-data <- read.csv("~/Documents/FE541/blockchain-github/bitcoin_data.csv")
-dxy <- read.csv("~/Documents/FE541/blockchain-github/dxy.txt")
-ir <- read.csv("~/Documents/FE541/blockchain-github/Inflation_rate.csv")
+bc <- read.csv("~/Documents/FE541/bitcoin-deflation/bit_usd.csv", header=TRUE)
+data <- read.csv("~/Documents/FE541/bitcoin-deflation/bitcoin_data.csv")
+dxy <- read.csv("~/Documents/FE541/bitcoin-deflation/dxy.txt")
+ir <- read.csv("~/Documents/FE541/bitcoin-deflation/Inflation_rate.csv")
 
 
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
-  
+ 
   # Generate a plot of the requested variable against mpg and only 
   # include outliers if requested
   output$Plot1 <- renderPlot({
@@ -30,26 +30,27 @@ shinyServer(function(input, output) {
   })
   output$Plot3 <- renderPlot({
     par(mfrow=c(2,2))
-    hist(bc$Close.Price)
-    hist(dxy$Close)
-    hist(log(dxy$Close))
-    hist(log(bc$Close.Price))
+    hist(bc$Close.Price, main="Bitcoin Close Price")
+    hist(dxy$Close, main="Dollar Index")
+    hist(log(bc$Close.Price), main="Log Bitcoin Close Price")
+    hist(log(dxy$Close), main="Log Dollar Index")
   })
   
   #adding the return to the bc data
   bc_Return <- diff(bc$Close.Price)/bc$Close.Price[-length(bc$Close.Price)]
   bc$bc_Return <- 0
   bc$bc_Return[2:length(bc$bc_Return)]<-bc_Return
-  output$Plot7 <- renderPlot({
-    hist(bc_Return)
-  })
   #adding the return to the dxy data
   dxy_Return <- diff(dxy$Close)/dxy$Close[-length(dxy$Close)]
   dxy$dxy_Return <- 0
   dxy$dxy_Return[2:length(dxy$dxy_Return)]<-dxy_Return
-  #output$Plot8 <- renderPlot({
-   # hist(dxy_Return)
-  #})
+  dxy_Return<-as.numeric(dxy_Return)
+  
+  output$Plot7 <- renderPlot({
+    par(mfrow=c(1,2))
+    hist(bc_Return, main="Histogram of Bitcoin close price return")
+    hist(dxy$dxy_Return, main="Histogram of Dollar index return")
+  })
   
   #change the date format
   bc$Date <- as.Date(bc$Date, origin = "2013-05-31")
@@ -78,12 +79,16 @@ shinyServer(function(input, output) {
   })
   #simple linear regression: bitcoin ~ dxy
   fit <- lm(merge$Close.Price~merge$Close)
-  summary(fit)
+  output$Print1 <-renderPrint({
+    summary(fit)
+  })
   output$Plot11 <- renderPlot({
     plot(merge$Close,merge$Close.Price)
     abline(fit,col="red")
   })
-  
+  output$Plot12 <- renderPlot({
+    plot(fit$residual~merge$Date)
+  })
   #time series analysis
   myvars <- c("Date","Close.Price","Close")
   bit <- merge[myvars]
@@ -92,13 +97,7 @@ shinyServer(function(input, output) {
   colnames(bit)[3] <- "dxy.close"
   #plot the time sesies data
   bit.zoo <- zoo(bit[,-1],order.by=bit$Date)
-  output$Plot12 <- renderPlot({
-    plot(bit.zoo,main="Daily Close Price - bit/usd and DXY")
-  })
-  output$Plot13 <- renderPlot({
-    #moving average of both price: to create a more smooth line
-    plot(rollapply(bit.zoo, width=20, mean, na.rm=T), main="Mean of Rolling 20 Obs.")
-  })
+
   #now perform regression analysis where dependent variable used bit/usd and dxy
   #is the indepedent variable
   tmp <- merge(bit.zoo[,"bit.close"], bit.zoo[,"dxy.close"])
@@ -126,8 +125,9 @@ shinyServer(function(input, output) {
   output$Plot16 <- renderPlot({
     plot(res, main="Coefficients of Regs.", xlab="Date")
   })
-  
-  summary(res)
+  output$Print2 <-renderPrint({
+    summary(res)
+  })
   output$Plot17 <- renderPlot({
     hist(res[,"dxy"], breaks=100, main="Histogram of Estimated Coefficients", xlab="Value")
   })
@@ -158,38 +158,24 @@ shinyServer(function(input, output) {
   output$Plot21<- renderPlot({
     tsdiag(arima(log(bit.ts), order = c(0,1,0)))
   })
-  
-  output$Plot22 <- renderPlot({
-    #ploting monthly ir graph
-    plot(ir$Date,ir$Inflation_rate,main="Monthly Inflation Rate",pch=1,col='black', cex=0.2,xlab='Date', ylab='Inflation_Rate')
-    lines(ir$Date,ir$Inflation_rate,col='black')
-  })
   inflation <- xts(ir[,-1], order.by=as.Date(ir[,1], "%m/%d/%Y"))
   
   # creating monthly bc and ir
   bc<- subset(bc,Date< as.Date("2016-11-01"))
   bchain <- xts(bc[,-1], order.by=as.Date(bc[,1], "%m/%d/%Y"))
   bchain<- bchain[complete.cases(bchain),]
-  x <- bchain$bc_Return
-  output$Plot23 <- renderPlot({
-    plot(x, main="Bitcoin Return Daily")
-  })
-  
-  bcm<- daily2monthly(x, FUN=mean, date.fmt ="%m/%Y", na.rm=TRUE)
-  output$Plot24 <- renderPlot({
-    plot(bcm, main="Bitcoin Return Monthly")
-  })
+  bcm<- daily2monthly(bchain$bc_Return, FUN=mean, date.fmt ="%m/%Y", na.rm=TRUE)
   
   dxy_Return<-subset(dxy,Date<as.Date("2016-11-01"),select = c(Date,dxy_Return))
   dxt <- xts(dxy_Return[,-1], order.by=as.Date(dxy_Return[,1], "%m/%d/%Y"))
-  output$Plot25 <- renderPlot({
-    plot(dxt, main="dxy Return Daily")
-  })
   dxm<- daily2monthly(dxt, FUN=mean, date.fmt ="%m/%Y", na.rm=TRUE)
+
   output$Plot26 <- renderPlot({
-    plot(dxm, main="dxy Return Monthly")
+    par(mfrow=c(2,2))
+    hist(ir$Inflation_rate, main="Histogram Monthly Inflation Rate")
+    hist(bcm, main="Histogram Monthly Bitcoin Return")
+    hist(dxm, main="Histogram Monthly dxy Return")
   })
-  
   data_bc<-xts(data[,-1],order.by=as.Date(data[,1], "%Y-%m-%d"))
   datam<-daily2monthly(data_bc, FUN=mean, date.fmt ="%m/%Y", na.rm=TRUE)
 
@@ -198,7 +184,7 @@ shinyServer(function(input, output) {
     plot(datam, main="Bitcoin monthly data", cex.lab = 1.5)
   })
   
-  write.csv(datam,'~/Documents/FE541/blockchain-github/datam.csv')
+  write.csv(datam,'~/Documents/FE541/bitcoin-deflation/datam.csv')
   #filtering ir on Date
   irt <- subset(ir,Date>=as.Date("2013-05-01"))
   merge1=merge(dxm,bcm,irt$Inflation_rate)
@@ -208,21 +194,15 @@ shinyServer(function(input, output) {
   })
   #cor(merge1)
   output$Plot29 <- renderPlot({
-    corrplot(cor(merge1), order = "hclust", tl.col='black', tl.cex=.75)
+    plot(ir~bcm, data=merge1, main="Inflation Rate to Bitcoin Return")
+    abline(ir.reg,col="red")
+    lines(lowess(merge1$ir~merge1$bcm),col="blue")
   })
-  
-  #plot.new()
-  par(mfrow=c(1,1))
-  output$Plot30 <- renderPlot({
-    par(mfrow=c(2,2))
-    hist(dxm)
-    hist(bcm)
-    hist(irt$Inflation_rate)
-  })
- 
   
   ir.reg<-lm(merge1$ir~merge1$bcm+merge1$dxm)
-  #anova(ir.reg)
+  output$Print3 <-renderPrint({
+    summary(ir.reg)
+  })
   par(mfrow=c(2,2))
   output$Plot33 <- renderPlot({
     par(mfrow=c(2,2))
@@ -238,15 +218,11 @@ shinyServer(function(input, output) {
   })
   
   #economic data
-  econ_data<- read.csv("~/Documents/FE541/blockchain-github/Total_data1.csv")
+  econ_data<- read.csv("~/Documents/FE541/bitcoin-deflation/Total_data1.csv")
   econ_data$Date <- as.Date(econ_data$Date,format="%m/%d/%Y")
   #str(econ_data)
   output$Plot34 <- renderPlot({
     corrplot(cor(econ_data[,-1]), order = "hclust", tl.col='black', tl.cex=.75)
-  })
-  
-  output$Plot35 <- renderPlot({
-    plot(econ_data$Date,econ_data$Inflation_Rate, pch=1 , col='black',main="Inflation Rate")
   })
   
   output$Plot36 <- renderPlot({
@@ -259,6 +235,56 @@ shinyServer(function(input, output) {
     lines(econ_data$Date, scale(econ_data$Trade_Weighted_Dollar),col="purple")
     legend("topleft", legend=c("Bitcoin_Est_Transaction_Vol_USD","Inflation_Rate","Global_Price_Brent_Crude","Customer_Price_Index","Trade_Weighted_Dollar"),col=c("black","blue","red","green","purple"),lw=3,cex=1,pt.cex=0.5)
   })
- 
   
+  output$Plot37 <- renderPlot({
+    hist(econ_data$Bitcoin_Est_Transaction_Vol_USD)
+  })
+  output$Plot38 <- renderPlot({
+  plot(econ_data$Inflation_Rate~econ_data$Bitcoin_Est_Transaction_Vol_USD,cex=0.5, main='Inflation Rate to Bitcoin transaction Vol USD')
+  abline(lm(econ_data$Inflation_Rate~econ_data$Bitcoin_Est_Transaction_Vol_USD),col='red')
+    })
+  output$Print4 <-renderPrint({
+    summary(lm(econ_data$Inflation_Rate~econ_data$Bitcoin_Est_Transaction_Vol_USD))
+  })
+  reg.model2<-lm(econ_data$Inflation_Rate~econ_data$Bitcoin_Est_Transaction_Vol_USD)
+  output$Plot39 <- renderPlot({
+    par(mfrow=c(2,2))
+    plot(lm(residuals(reg.model2)~fitted(reg.model2)),col='red')
+  })
+  mod_gam <- gam(econ_data$Inflation_Rate ~ s(econ_data$Bitcoin_Est_Transaction_Vol_USD,bs="cr"))
+  output$Plot40 <- renderPlot({
+    plot(mod_gam, pages=1, residuals=T, pch=19, cex=0.25,
+         scheme=1, col='#FF8000', shade=T,shade.col='gray90') 
+  })
+  output$Print5 <-renderPrint({
+    summary(mod_gam)
+  })
+  library(nFactors)
+  ev <- eigen(cor(econ_data[,c(-1,-2)])) # get eigenvalues
+  ap <- parallel(subject=nrow(econ_data[,c(-1,-2)]),var=ncol(econ_data[,c(-1,-2)]),
+                 rep=100,cent=.05)
+  nS <- nScree(x=ev$values, aparallel=ap$eigen$qevpea)
+  output$Plot41 <- renderPlot({
+    plotnScree(nS) 
+  })
+  f3w <- fa(econ_data[,c(-1,-2)],2,n.obs = 100,fm="wls")
+  output$Plot42 <- renderPlot({
+    #plot(f3w)
+    fa.diagram(f3w)
+  })
+  SumScale1<-(Producer_Price_Index+Export+Import+Customer_Price_Index+ 
+                Global_Price_Brent_Crude + (10-Trade_Weighted_Dollar)+
+                (10-+Bitcoin_Est_Transaction_Vol_USD))/7
+  SumScale2<-(Total_Bitcoin+Bitcoin_market_Cap+ 
+                Bitcoin_trade_Vol_VS_Transaction_Vol+Personal_Consumption_Expenditure)/4
+  output$Plot43 <- renderPlot({
+    par(mfrow=c(1,2))
+    hist(SumScale1)
+    hist(SumScale2)
+  })
+  model.reg<-lm(Inflation_Rate~SumScale1+SumScale2)
+  output$Print6 <-renderPrint({
+    summary(lm(Inflation_Rate~SumScale1+SumScale2))
+  })
+   
 })
--- Ui.R
+++ Ui.R
@@ -5,45 +5,121 @@ shinyUI(pageWithSidebar(
   
   # Application title
   headerPanel("Deflationary effect of Bitcoin on US economy "),
-  
+ 
   # Sidebar with controls to select the variable to plot against mpg
   # and to specify whether outliers should be included
   sidebarPanel(
-
-  ),
-  
+    span("This project was done by"), 
+    a("Mehrnoosh Oghbaie", href="https://github.com/moghbaie/bitcoin-deflation", target="_blank") , 
+    span("& Hongjing Zhang as part of the"), 
+    a("applied statistics with application in finance", href="https://github.com/moghbaie/bitcoin-deflation", target="_blank"),span("course at Stevens Institute of Technology.")
+    ),
+ 
   # Show the caption and plot of the requested variable against mpg
   mainPanel(
-    plotOutput("Plot1"),
-    plotOutput("Plot2"),
-    plotOutput("Plot3"),
-    plotOutput("Plot7"),
-    #plotOutput("Plot8"),
+    br(),
+    br(),
+    p("The goal of this project was to investigate the deflationary effect of Bitcoin on US economy. Some economists, such as Paul Krugman believed in the possibility of deflationary pressure in crypto-currency networks. Bitcoin’s capped total money supply could be viewed as a variation on Milton Friedmans “k-percent rule”. This theory states that an optimal way to control inflation over the long term is for the central bank to grow the money supply by a fixed amount of k% each year, irrespective of the cyclical state of the economy. In particular, one should set the growth variable of k% at a rate equal to the growth of real GDP each year. This connection between Milton Freidman’s Nobel prize winning theory and Bitcoin practice was highlighted recently in a paper by B¨ohme who argues that one can consider Bitcoin as a type of “... proposal to fix the annual growth rate of the money supply to a fixed rate of growth.” B¨ohme reiterates the views of Paul Krugman that “... the fixed slow growth rate of Bitcoin creates the possibility of deflation if Bitcoin was to be used widely...”. He also notes that there have been other crypto-currency extensions of Bitcoin proposed to overcome such potential problems, see discussion by for instance King which introduces Primecoin with infinite money supply or the introduction of Peercoin which keeps k% around 1-2."),
+    h2("Bitcoin price vs. US dollar index"),
+    p("US dollar index is a good indicator of how strong US economy is. We studied the relationship between Bitcoin price and US dollar index.
+While the US dollar is a stable currency, the price of the bitcoin compare to the US dollar reveals how investors value the virtual currency and how the speculation formed during time.
+      The DXY is a convenient index to measure the strength and weakness of the US dollar, JP Morgan created the DXY in 1973, and it has only been updated once, for the introduction of the Euro currency. We explored some of the features of the DXY and the BIT/USD such as the histogram on raw data and histogram on log (data). We put both measures in the same graph, and build a linear regression model. Then we did a simple time series analysis on the data."),
+    h4("Data:"),
+    div("Daily close price of BIT/USD from 2013-05-31 to 2016-11-17"),
+    div("Daily return of close price of BIT/USD from 2013-05-31 to 2016-11-17"),
+    div("Daily close price of the US dollar index from the same time span (DXY)"),
+    div("Daily return of close price of US dollar index from the same time span"),
     plotOutput("Plot9"),
+    p("The negative relationship between Bitcoin price and Dollar index is more obvious after we draw them together."),
+    br(),
+    plotOutput("Plot3"),
+    p("The histogram of the daily close price of BIT/USD appears to be slightly right-skewed. The log transformation distribution is even more complicated (with no single peak and left skewed)."),
+    p("The histogram of the daily DXY appears to be bivariate normal, and stays the same after log transformation."),
+    br(),
+    plotOutput("Plot7"),
+    p("Histogram of Bitcoin return appears to be more positive than negative (the bar that is positive is slightly higher than the one that’s negative), this matches the fact that despite a short period of price falling, Bitcoin value is increasing with regards to USD, which is a rather stable currency."),
+    p("Histogram of US dollar index appears to have more negative returns than positive returns, indicating that the US dollar is possibly going weaker and weaker over time."),
     plotOutput("Plot10"),
+    p("The return of the bitcoin is much more volatile than the US dollar index which coincides with the reality that bitcoin still remains as a very speculative asset in the market. 
+Also we can see that the returns appear to have no pattern, which proves the complete market hypothesis: it is impossible to predict the market and make profit of it. Of course there will be no financial market if there appears zero arbitrage opportunities, but speaking from the broad aspect, the market is kind of complete.
+      "),
+    verbatimTextOutput("Print1"),
+    h4("Result:"),
+    HTML("<ol class='circle-list'><li><p>The p-values of the intercept and the slope are both significant at a 95% confidence level, which indicates that there appears to be a relationship between these two currencies.</p>
+      </li>
+      <li>
+      <p>The coefficient of the slope is negative, so we make the statement that when the DXY goes down, the BIT/USD will go up.</p>
+      </li>
+      <li>
+      <p>The R-square is only 0.01564, which means that only 1.5% percent of the change in BIT/USD can be explained by the change in DXY. This is strong evidence against the statement we made in 1 and 2. </p>
+      </li>
+      </ol> "),
     plotOutput("Plot11"),
+    p("The residual against the X-axis plot suggests that the residuals are not randomly distributed, which violates the assumption of a randomly distributed residuals."),
     plotOutput("Plot12"),
-    plotOutput("Plot13"),
-    plotOutput("Plot14"),
-    plotOutput("Plot15"),
+    p("residuals plot shows that there is a volatility cluttering at certain time periods, responding to certain period of economy where there are some major events happening."),
     plotOutput("Plot16"),
+    p("We did time series analysis, which differs from the linear regression in that the coefficient and the intercept do not stay constant: they evolve over time."),
+    verbatimTextOutput("Print2"),
     plotOutput("Plot17"),
-    plotOutput("Plot18"),
-    plotOutput("Plot19"),
-    plotOutput("Plot20"),
-    plotOutput("Plot21"),
-    plotOutput("Plot22"),
-    plotOutput("Plot23"),
-    plotOutput("Plot24"),
-    plotOutput("Plot25"),
-    plotOutput("Plot26"),
-    plotOutput("Plot27"),
+    p("From the histogram of the estimated coefficients we can see that the negative ones outnumber the positive ones, hence we are certai that the DXY and BIT/USD do have a negative relationship with each other."),
+    br(),
+    h2("US inflation rate vs. Bitcoin return and DXY return"),
+    br(),
+    h4("Data:"),
+    div("Monthly return on close price of BIT/USD from 2013-05-01 to 2016-11-01"),
+    div("Monthly return on DXY from 2013-05-01 to 2016-11-01"),
+    div("Monthly return on US inflation rate from 2013-05-01 to 2016-11-01"),
+    br(),
+    p("To investigate the effect of Bitcoin on inflation rate, we transform the Bitcoin return and DXY return to monthly data, and tried to build a multiple linear regression model. Our model was supposed to show the relationship between inflation rate and Bitcoin return and DXY return."),
     plotOutput("Plot28"),
+    plotOutput("Plot26"),
+    p("Monthly inflation rate and monthly dxy return follow normal distribution. Bitcoin return is normally distributed but has outlier."),
+    verbatimTextOutput("Print3"),
+    p("The multiple regression model shows that there is no significant relationship between inflation rate and Bitcoin or DXY return, the R-squared is extremely low and linear model is far from ideal. Residuals don’t seem to be normally distributed"),
     plotOutput("Plot29"),
-    plotOutput("Plot30"),
     plotOutput("Plot33"),
+    h2("Relationship between Bitcoin measures and economy indicators:"),
+    p("We decided to use more data related to bitcoin Volume and its transaction to US dollar and economic indicators including inflation rate to study their correlation and possible relationship."),
+    br(),
+    h4("Data:"),
+    p("We downloaded Bitcoin data from Quandle and Economy related data from Fred:"),
+    br(),
+    div("Monthly total bitcoin BCHAIN/TOTBC from 2013-05-31 to 2016-11-17"),
+    div("Monthly Bitcoin USD Exchange Trade Volume BCHAIN/ETRAV from 2013-05-31 to 2016-11-17"),
+    div("Monthly Bitcoin Market Capitalization BCHAIN/MKTCP from 2013-05-31 to 2016-11-17"),
+    div("Monthly Bitcoin Trade Volume vs. Transaction Volume Ratio BCHAIN/TVTVR from 2013-05-31 to 2016-11-17"),
+    div("Monthly US inflation Rate from 2013-05-31 to 2016-11-17"),
+    div("Monthly Producer Price Index from 2013-05-31 to 2016-11-17"),
+    div("Monthly Customer Price Index from 2013-05-31 to 2016-11-17"),
+    div("Monthly Global price Brent Crude from 2013-05-31 to 2016-11-17"),
+    div("Monthly Export from 2013-05-31 to 2016-11-17"),
+    div("Monthly Import from 2013-05-31 to 2016-11-17"),
+    div("Monthly Personal Consumption Expenditure from 2013-05-31 to 2016-11-17"),
+    br(),
+    h4("Analysis:"),
+    p("As we made a correlation plot between all the factors, we found strong negative correlation between Bitcoin estimated transaction volume in USD and inflation rate. "),
+    br(),
     plotOutput("Plot34"),
-    plotOutput("Plot35"),
-    plotOutput("Plot36")
-  )
+    br(),
+    p("Building a comparison scaled plot helped us to follow the changes and correlation as shown below. There is positive correlation between global price Brent crude and inflation rate and also negative correlation between Bitcoin estimated transaction volume in USD and inflation rate. Other Bitcoin measures seem to be negatively correlated too."),
+    plotOutput("Plot36"),
+    plotOutput("Plot37"),
+    p("Bitcoin estimated transaction volume in USD doesn’t have an ideal normal distribution. Linear regression model shows obvious significant relationship between inflation rate and Bitcoin estimated transaction volume in USD. P-values are small and R-squared is around 0.45."),
+    plotOutput("Plot38"),
+    verbatimTextOutput("Print4"),
+    p("Residuals seem to be normally distributed. Residuals/ fitted values graph show concentration in around low values that could be caused by instability of Bitcoin in early days."),
+    plotOutput("Plot39"),
+    p("Building the gam model we got slightly better R-square and significant p-value as shown below. Residuals seem to be normally distributed."),
+    plotOutput("Plot40"),
+    verbatimTextOutput("Print5"),
+    p("We also did a factor analysis to find out how our indicators are related to inflation rate. Factor analysis suggested us using two scales."),
+    plotOutput("Plot41"),
+    plotOutput("Plot42"),
+    plotOutput("Plot43"),
+    p("Both scales don’t seem to have normal distribution. After building our scales, we build a linear regression model to see how different scales contributing to inflation rate. Sumscale2 doesn’t to be very significant."),
+    verbatimTextOutput("Print6"),
+    h2("Conclusion:"),
+    p("Bitcoin still remains very volatile in the financial market because of its virtual nature. Compared with it the US dollar index has a much lower return rate in general.Despite its economical dependence, the BIT/USD and DXY does not appear to have a statistically significant relationship among them, if there exists a relationship, it is a rather weak one, and they are negatively dependent on each other, meaning if one goes up, the other one goes down. Also in the end the time series analysis method is brought up, suggesting a possible solution for depicting the relationship between the virtual currency bitcoin and so far the most stable currency in the world – the US dollar. There is significant negative relationship between Bitcoin estimated transaction volume in USD and US inflation rate, which proves our hypothesis, was right. We also found that some of our indicators are more related to inflation rate than others, which could be good base for future research.")
+    )
 ))
